7166ea185c34419218965edb4fccc23112fb9c7b
Kaja Sai Chandana <31963793+kajachandana@users.noreply.github.com>
2018-11-21 18:24:00 +0530

    Create Prime_factor.c

---
+++ Prime_factor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int solutions (int a, int b)
+    {
+     int cnt = 0;
+     for (int i=a; i<=b; i++)
+      
+       for (int j=1; j*j<= i; j++)
+        if (j*j== i)
+        cnt++;
+        return cnt;
+    }
+    
+}
