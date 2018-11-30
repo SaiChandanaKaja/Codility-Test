4d8ff4125d4fde4f40d539a455cd5520397419b8
Kaja Sai Chandana <31963793+kajachandana@users.noreply.github.com>
2018-11-30 17:23:18 +0530

    Create Dice-game.c

---
+++ Dice-game.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int g1, g2, g3, g4, gSum, e1, e2, e3, e4, eSum;
+	scanf("%d %d %d %d", &g1, &g2, &g3, &g4);
+	scanf("%d %d %d %d", &e1, &e2, &e3, &e4);
+
+	gSum = g1+g2+g3+g4;
+	eSum = e1+e2+e3+e4;
+
+	if(gSum > eSum) {
+        printf("Gunnar");
+	} else if (eSum > gSum) {
+        printf("Emma");
+	} else{
+        printf("Tie");
+	}
+	
+
+	return 0;
+}
